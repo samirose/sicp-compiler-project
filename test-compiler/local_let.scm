0d1e9a54b311bff1b3e926f7366236667a0838f7
Sami Rosendahl <sami.rosendahl@gmail.com>
2020-10-28 22:01:08 +0200

    Fix a bug in let implementation and add more tests;
    The bug caused the local definitions generating code to carry over
    code from the previous expression's result.

--- test-compiler/local_let.scm
+++ test-compiler/local_let.scm
@@ -1,6 +1,47 @@
-(begin
-  (define (prodsum x y)
-    (let ((prod (* x y))
-          (double (lambda (x) (+ x x))))
-      (double prod)))
-  (prodsum 2 3))
+(define-library
+  (export constant-let
+          expression-let
+          multi-variable-let
+          let-in-lambda
+          let-value-referring-lambda-arg
+          let-value-shadowing-lambda-arg
+          lambda-as-let-value)
+
+  (begin
+    (define (constant-let)
+      (let ((a 2))
+        (+ 40 a)))
+
+    (define (expression-let)
+      (let ((a (* 2 3)))
+        (* 5 a)))
+
+    (define (multi-variable-let)
+      (let ((a 2) (b 3))
+        (* a b)))
+
+    (define (let-in-lambda)
+      ((lambda (x)
+         (let ((a 2))
+           (* a x)))
+       5))
+
+    (define (let-value-referring-lambda-arg)
+      ((lambda (x)
+         (let ((a (* x 2)))
+           (* a x)))
+       3))
+
+    (define (let-value-shadowing-lambda-arg)
+      ((lambda (x)
+         (let ((x (* x 2)))
+           (* x x)))
+       3))
+
+    (define (lambda-as-let-value)
+      (let ((prod (* 2 3))
+            (double (lambda (x) (+ x x))))
+        (double prod)))
+
+    )
+  )

23da95da080d757f1b6b4b8e39954efd0bd31b24
Sami Rosendahl <sami.rosendahl@gmail.com>
2021-01-05 22:24:52 +0200

    Reformat arithmetic operators test

--- test-compiler/arithmetic_operators.scm
+++ test-compiler/arithmetic_operators.scm
@@ -1,33 +1,33 @@
 (define-library
-    (export
-        plus-identity
-        multiply-identity
-        plus-single
-        minus-single
-        multiply-single
-        plus-zero-to-positive
-        plus-positive-result
-        signed-plus-positive-result
-        signed-plus-zero-result
-        signed-plus-negative-result
-        plus-zero-to-negative
-        plus-multiple
-        complex)
+  (export
+   plus-identity
+   multiply-identity
+   plus-single
+   minus-single
+   multiply-single
+   plus-zero-to-positive
+   plus-positive-result
+   signed-plus-positive-result
+   signed-plus-zero-result
+   signed-plus-negative-result
+   plus-zero-to-negative
+   plus-multiple
+   complex)
 
-    (begin
-        (define (plus-identity) (+))
-        (define (multiply-identity) (*))
-        (define (plus-single) (+ 2))
-        (define (minus-single) (- 2))
-        (define (multiply-single) (* 2))
-        (define (plus-zero-to-positive) (+ 0 2))
-        (define (plus-positive-result) (+ 1 2))
-        (define (signed-plus-positive-result) (+ 2 -1))
-        (define (signed-plus-zero-result) (+ 2 -2))
-        (define (signed-plus-negative-result) (+ 2 -3))
-        (define (plus-zero-to-negative) (+ -2 0))
-        (define (plus-multiple) (+ 1 2 3))
+  (begin
+    (define (plus-identity) (+))
+    (define (multiply-identity) (*))
+    (define (plus-single) (+ 2))
+    (define (minus-single) (- 2))
+    (define (multiply-single) (* 2))
+    (define (plus-zero-to-positive) (+ 0 2))
+    (define (plus-positive-result) (+ 1 2))
+    (define (signed-plus-positive-result) (+ 2 -1))
+    (define (signed-plus-zero-result) (+ 2 -2))
+    (define (signed-plus-negative-result) (+ 2 -3))
+    (define (plus-zero-to-negative) (+ -2 0))
+    (define (plus-multiple) (+ 1 2 3))
 
-        (define (complex) (+ (/ (* 2 -6 2) 3 2) (- 10 10 6) 2))
+    (define (complex) (+ (/ (* 2 -6 2) 3 2) (- 10 10 6) 2))
     )
-)
+  )

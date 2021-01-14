38569dc8a7872b645b1c1df844795e3db8513ead
Sami Rosendahl <sami.rosendahl@gmail.com>
2021-01-14 22:27:14 +0200

    Add test for special symbol override with variable

---
+++ test-compiler/override_special_form_as_variable.scm
@@ -0,0 +1,37 @@
+(define-library
+  (export override-quote-let
+          override-quote-arg
+          override-define-let
+          override-define-arg
+          override-lambda-let
+          override-lambda-arg
+          override-special-symbols-let*)
+
+  (begin
+    (define (override-quote-let)
+      (let ((quote (+ 40 2)))
+        quote))
+
+    (define (override-quote-arg quote)
+      (+ quote 1))
+
+    (define (override-define-let)
+      (let ((define 42))
+        define))
+
+    (define (override-define-arg define)
+      (+ define 1))
+
+    (define (override-lambda-let)
+      (let ((lambda (+ 40 2)))
+        lambda))
+
+    (define (override-lambda-arg lambda)
+      (+ lambda 1))
+
+    (define (override-special-symbols-let*)
+      (let* ((if 40)
+             (set! 2))
+        (+ if set!)))
+  )
+)

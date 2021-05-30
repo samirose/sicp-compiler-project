9bbaf281af82f0740c31102c2558c402c329fe0f
Sami Rosendahl <sami.rosendahl@gmail.com>
2021-05-30 22:54:16 +0300

    Combine lambda argument shadowing test to lambda test suite

--- test-compiler/lambda.scm
+++ test-compiler/lambda.scm
@@ -1,10 +1,12 @@
 (define-library
 
+  (import (scheme base))
+
   (export lambda-application
           lambda-argument
-          lambda-without-parameters)
-
-  (import (scheme base))
+          lambda-without-parameters
+          lambda-parameters-shadow-globals
+          inner-lambda-parameters-shadow-outer)
 
   (begin
     (define (lambda-application)
@@ -17,5 +19,14 @@
        5))
 
     (define (lambda-without-parameters)
-      ((lambda () (+ 40 2)))))
+      ((lambda () (+ 40 2))))
+
+    (define g 2)
+    (define h 3)
+
+    (define (lambda-parameters-shadow-globals)
+      ((lambda (g) (+ g h)) 4))
+
+    (define (inner-lambda-parameters-shadow-outer)
+      ((lambda (x) (+ 2 ((lambda (x) (* x x)) 3))) 4)))
 )
--- test-compiler/local_shadows_global.scm
+++
@@ -1,8 +0,0 @@
-(define-library
-
-  (import (scheme base))
-
-  (begin
-    (define x 40)
-    (define y 2)
-    ((lambda (x z) (+ x z)) 3 x)))

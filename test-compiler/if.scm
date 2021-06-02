25f89dd99d747caedd511a80a154e3c2eed56cec
Sami Rosendahl <sami.rosendahl@gmail.com>
2021-06-02 21:31:10 +0300

    Improve if expression test

--- test-compiler/if.scm
+++ test-compiler/if.scm
@@ -1,10 +1,14 @@
 (define-library
   (export if-true
-          if-true-consquent-only
+          if-true-expression
+          if-true-does-not-evaluate-alternative
+          if-true-consequent-only
           if-false
-          if-false-consquent-only
+          if-false-expression
+          if-false-does-not-evaluate-consequent
+          if-false-consequent-only
           if-lambda-condition
-          if-lambda-consquent
+          if-lambda-consequent
           if-lambda-alternative)
 
   (import (scheme base))
@@ -13,19 +17,33 @@
     (define x 1)
 
     (define (if-true)
+      (if #t 1 2))
+
+    (define (if-true-expression)
       (set! x 1)
       (if (= x 1) x (+ x 1)))
 
-    (define (if-true-consquent-only)
+    (define (if-true-does-not-evaluate-alternative)
+      (set! x 1)
+      (if (= x 1) x (set! x 2)))
+
+    (define (if-true-consequent-only)
       (set! x 1)
       (if (= x 1) (set! x (+ x 2)))
       x)
 
     (define (if-false)
+      (if #f 1 2))
+
+    (define (if-false-expression)
       (set! x 1)
       (if (> x 1) x (+ x 1)))
 
-    (define (if-false-consquent-only)
+    (define (if-false-does-not-evaluate-consequent)
+      (set! x 1)
+      (if (= x 0) (set! x 2) x))
+
+    (define (if-false-consequent-only)
       (set! x 1)
       (if (> x 1) (set! x (+ x 2)))
       x)
@@ -34,7 +52,7 @@
       (if ((lambda (x) (> x 0)) 1)
           1 2))
 
-    (define (if-lambda-consquent)
+    (define (if-lambda-consequent)
       (if ((lambda (x) (> x 0)) 1)
           ((lambda (x) (+ x 1)) 1)
           3))

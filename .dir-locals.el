d6850f979b0b1348fdaf175236380631a429f7f5
Sami Rosendahl <sami.rosendahl@gmail.com>
2022-03-29 23:27:26 +0300

    Set emacs dir locals for Geiser & Guile

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((geiser-scheme-implementation . guile)))
+ (scheme-mode . ((geiser-guile-binary . ("guile" "--r7rs"))))
+ ("test-unit" . ((scheme-mode . ((geiser-guile-load-path . ".."))))))

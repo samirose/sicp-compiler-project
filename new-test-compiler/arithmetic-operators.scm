1917203f9fb839b7546aedefb63178219f1f968f
Sami Rosendahl <sami.rosendahl@gmail.com>
2022-08-25 22:53:28 +0300

    Refine compiler-test library

--- new-test-compiler/arithmetic-operators.scm
+++ new-test-compiler/arithmetic-operators.scm
@@ -1,21 +1,34 @@
 (import (scheme base)
 	(compiler-test))
 
-(begin-compiler-test "arithmetic-operators")
+(compiler-test-begin "arithmetic-operators")
 
-(test-eq "(+) is zero" 0 (+))
-(test-eq "(*) is one" 1 (*))
-(test-eq "(+ x) is x" 2 (+ 2))
-(test-eq "(- x) is -x" -2 (- 2))
-(test-eq "(* x) is x" 2 (* 2))
-(test-eq "(+ 0 x) is x, x > 0" 2 (+ 0 2))
-(test-eq "(+ 0 x) is x, x < 0" -2 (+ -2 0))
-(test-eq "(+ x y) is x + y (x > 0, y > 0)" 3 (+ 1 2))
-(test-eq "(+ x y) is x + y (x > 0, y < 0, |x| > |y|)" 1 (+ 2 -1))
-(test-eq "(+ x y) is x + y (x > 0, y < 0, |x| < |y|)" -1 (+ 2 -3))
-(test-eq "(+ a b c) is a + b + c" 6 (+ 1 2 3))
-(test-eq "complex arithmetic expression evaluates to correct result"
+(compiler-test-eq plus-identity "(+) is zero"
+		  0 (+))
+(compiler-test-eq multiply-identity "(*) is one"
+		  1 (*))
+(compiler-test-eq plus-single "(+ x) is x"
+		  2 (+ 2))
+(compiler-test-eq minus-single "(- x) is -x"
+		  -2 (- 2))
+(compiler-test-eq multiply-single "(* x) is x"
+		  2 (* 2))
+(compiler-test-eq plus-zero-to-positive "(+ 0 x) is x, x > 0"
+		  2 (+ 0 2))
+(compiler-test-eq plus-zero-to-negative "(+ 0 x) is x, x < 0"
+		  -2 (+ -2 0))
+(compiler-test-eq plus-positive-result "(+ x y) is x + y (x > 0, y > 0)"
+		  3 (+ 1 2))
+(compiler-test-eq signed-plus-positive-result "(+ x y) is x + y (x > 0, y < 0, |x| > |y|)"
+		  1 (+ 2 -1))
+(compiler-test-eq signed-plus-zero-result "(+ x y) is 0 (x = -y)"
+		  0 (+ 2 -2))
+(compiler-test-eq signed-plus-negative-result "(+ x y) is x + y (x > 0, y < 0, |x| < |y|)"
+		  -1 (+ 2 -3))
+(compiler-test-eq plus-multiple "(+ a b c) is a + b + c"
+		  6 (+ 1 2 3))
+(compiler-test-eq complex "complex arithmetic expression evaluates to correct result"
   -8
   (+ (/ (* 2 -6 2) 3 2) (- (+ 1 2 3)) (* (+ (+ 1 1)))))
 
-(end-compiler-test "arithmetic-operators")
+(compiler-test-end "arithmetic-operators")
--- new-test-compiler/lib/compiler-test.scm
+++ new-test-compiler/lib/compiler-test.scm
@@ -1,19 +1,24 @@
 (define-library
     (compiler-test)
 
-  (export begin-compiler-test
-	  end-compiler-test
-	  test-eq)
+  (export compiler-test-begin
+	  compiler-test-end
+	  compiler-test-eq)
 
   (import (scheme base)
 	  (scheme process-context)
 	  (srfi srfi-64))
 
   (begin
-    (define (begin-compiler-test name)
+    (define (compiler-test-begin name)
       (test-begin name))
 
-    (define (end-compiler-test name)
+    (define-syntax compiler-test-eq
+      (syntax-rules ()
+	((compiler-test-eq name-symbol desc . rest)
+	 (test-eq desc . rest))))
+
+    (define (compiler-test-end name)
       (let ((success (= 0 (test-runner-fail-count (test-runner-current)))))
 	(test-end name)
 	(exit success)))))

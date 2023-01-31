0576c60d6a316c24c03104f144663ce6380a0139
Sami Rosendahl <sami.rosendahl@gmail.com>
2023-01-31 21:49:28 +0200

    Extend exported-procedure compiler test

--- test-compiler/exported-procedure.scm
+++ test-compiler/exported-procedure.scm
@@ -1,10 +1,12 @@
 (define-library 
   (exported-procedure)
 
-  (export square minus)
+  (export value square minus double)
 
   (import (scheme base))
 
   (begin
+    (define (value) 42)
     (define (square x) (* x x))
-    (define (minus x y) (- x y))))
+    (define (minus x y) (- x y))
+    (define double (lambda (x) (+ x x)))))

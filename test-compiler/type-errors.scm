d207a857210672094413792261b837ac8f960e0a
Sami Rosendahl <sami.rosendahl@gmail.com>
2023-04-18 22:31:19 +0300

    Add more type error tests

--- test-compiler/type-errors.scm
+++ test-compiler/type-errors.scm
@@ -3,7 +3,12 @@
 
   (export
    plus-two
-   two-times)
+   two-times
+   lambda-two-times
+   procedure-plus-two
+   apply-to
+   unspecified-plus-two
+   unspecified-as-operator)
 
   (import (scheme base))
 
@@ -13,5 +18,21 @@
 
     (define (two-times x)
       (* 2 x))
+
+    (define (lambda-two-times)
+      (two-times (lambda (x) (* x x))))
+
+    (define (procedure-plus-two)
+      (plus-two two-times))
+
+    (define (apply-to o p)
+      (o p))
+
+    (define (unspecified-plus-two)
+      (plus-two (cond (#f 42))))
+
+    (define (unspecified-as-operator x)
+      ((cond (#f (lambda (x) (+ x 1)))) x))
+
     )
   )

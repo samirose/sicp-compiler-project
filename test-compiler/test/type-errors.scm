df444f03ebff972253be70551a01ba271ba11b49
Sami Rosendahl <sami.rosendahl@gmail.com>
2023-04-28 23:28:35 +0300

    Add runtime error type checking to WAST compiler tests

--- test-compiler/test/type-errors.scm
+++ test-compiler/test/type-errors.scm
@@ -5,34 +5,34 @@
 
 (compiler-test-error
  "arithmetic with boolean value is a type error"
- (plus-two #t))
+ (plus-two #t) expected-number)
 
 (compiler-test-error
  "arithmetic with boolean value is a type error"
- (plus-two #f))
+ (plus-two #f) expected-number)
 
 (compiler-test-error
  "arithmetic with procedure value is a type error"
- (lambda-two-times))
+ (lambda-two-times) expected-number)
 
 (compiler-test-error
  "arithmetic with procedure value is a type error"
- (procedure-plus-two))
+ (procedure-plus-two) expected-number)
 
 (compiler-test-error
  "number as operator is a type error"
- (apply-to 2 #f))
+ (apply-to 2 #f) expected-procedure)
 
 (compiler-test-error
  "boolean as operator is a type error"
- (apply-to #t 3))
+ (apply-to #t 3) expected-procedure)
 
 (compiler-test-error
  "arithmetic with unspecified value is a type error"
- (unspecified-plus-two))
+ (unspecified-plus-two) expected-number)
 
 (compiler-test-error
  "unspecified value as operator is a type error"
- (unspecified-as-operator 1))
+ (unspecified-as-operator 1) expected-procedure)
 
 (compiler-test-end "type-errors")

aaa519649e91dff4eeb378d224bc1d263e5628af
Sami Rosendahl <sami.rosendahl@gmail.com>
2024-01-03 22:20:23 +0200

    Add compile-command definition for Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
-((nil . ((geiser-scheme-implementation . guile)))
+((nil . ((geiser-scheme-implementation . guile)
+         (compile-command . "gmake -k -j -O test")))
  (scheme-mode . ((geiser-guile-binary . ("guile" "--r7rs"))
 		 (geiser-guile-load-path . ("./test-compiler" "./test-compiler/lib")))))
